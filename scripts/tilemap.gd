d77549ee2650fde3aa73004df4f384a432145e46
Jcodefox <jcodefox@gmail.com>
2023-06-11 22:27:51 -0400

    Turns out the placement system is quite simplifiable.

--- scripts/tilemap.gd
+++ scripts/tilemap.gd
@@ -3,30 +3,21 @@ extends TileMap
 @export var stand: PackedScene
 
 class Tile:
-	var render_src: Texture2D
 	var tileset_location: Vector2i
 	var tileset_src_id: int
-	var tilemap_layer: int
-	var price: int
 	var render_offset: Vector2i
-	func _init(rndr_src, set_location, set_src_id, map_layer, prc, rndr_offset = Vector2i.ZERO):
+	func _init(set_location, set_src_id, rndr_offset = Vector2i.ZERO):
 		tileset_location = set_location
 		tileset_src_id = set_src_id
-		tilemap_layer = map_layer
-		price = prc
-		render_src = rndr_src
 		render_offset = rndr_offset
 
-const GROUND_TILESET: Texture2D = preload("res://sprites/tiles_packed_1.png")
-const STANDS_TILESET: Texture2D = preload("res://sprites/tiles_packed_1.png")
-
 var placeable_tiles: Array[Tile] = [
-	Tile.new(GROUND_TILESET, Vector2i(16, 3), 0, 2, 5), # Walkable
-	Tile.new(GROUND_TILESET, Vector2i(1, 5), 0, 2, 5, Vector2i(0, -1)), # Wall
-	Tile.new(STANDS_TILESET, Vector2i(0, 0), 1, 3, 100), # Battery
-	Tile.new(STANDS_TILESET, Vector2i(2, 0), 1, 3, 100), # Windup
-	Tile.new(STANDS_TILESET, Vector2i(0, 2), 1, 3, 100), # Water
-	Tile.new(STANDS_TILESET, Vector2i(2, 2), 1, 3, 100), # Bread
+	Tile.new(Vector2i(16, 3), 0), # Walkable
+	Tile.new(Vector2i(1, 5), 0, Vector2i(0, -1)), # Wall
+	Tile.new(Vector2i(0, 0), 1), # Battery
+	Tile.new(Vector2i(2, 0), 1), # Windup
+	Tile.new(Vector2i(0, 2), 1), # Water
+	Tile.new(Vector2i(2, 2), 1), # Bread
 ]
 
 var selected_structure: int = 0
@@ -51,8 +42,11 @@ func update_Cursor():
 	if selected_structure < 0:
 		selected_structure = placeable_tiles.size() - 1
 	selected_structure = selected_structure % placeable_tiles.size()
-	$Cursor.texture = placeable_tiles[selected_structure].render_src
-	$Cursor.region_rect = tile_set.get_source(placeable_tiles[selected_structure].tileset_src_id).get_tile_texture_region(placeable_tiles[selected_structure].tileset_location)
+	var selected_tile: Tile = placeable_tiles[selected_structure]
+	var tileset_src: TileSetSource = tile_set.get_source(selected_tile.tileset_src_id)
+	$Cursor.texture = tileset_src.texture
+	$Cursor.region_rect = tileset_src.get_tile_texture_region(selected_tile.tileset_location)
+	
 
 func _process(delta: float) -> void:
 	var mouse_pos: Vector2i = get_global_mouse_position() / 16
@@ -67,16 +61,20 @@ func _process(delta: float) -> void:
 		return
 	
 	var selected_tile: Tile = placeable_tiles[selected_structure]
-	var cell_pointed_at: Vector2i = get_cell_atlas_coords(selected_tile.tilemap_layer, mouse_pos)
+	var tile_data: TileData = tile_set.get_source(selected_tile.tileset_src_id).get_tile_data(selected_tile.tileset_location, 0)
+	var layer: int = tile_data.get_custom_data("place_layer")
+	var cell_pointed_at: Vector2i = get_cell_atlas_coords(layer, mouse_pos)
 	var cell_duplicate: bool = cell_pointed_at == selected_tile.tileset_location
 	if Input.is_action_pressed("place") and not cell_duplicate:
-		if GlobalData.purchase(selected_tile.price, mouse_pos * 16):
-			dig(mouse_pos, selected_tile.tilemap_layer, 5)
-			set_cell(selected_tile.tilemap_layer, mouse_pos, selected_tile.tileset_src_id, selected_tile.tileset_location)
+		var price: int = tile_data.get_custom_data("price")
+		if GlobalData.purchase(price, mouse_pos * 16):
+			dig(mouse_pos, layer)
+			set_cell(layer, mouse_pos, selected_tile.tileset_src_id, selected_tile.tileset_location)
 	if Input.is_action_pressed("dig"):
-		dig(mouse_pos, selected_tile.tilemap_layer, 5)
+		dig(mouse_pos, layer)
 
-func dig(location: Vector2i, layer: int, price: int):
-	if get_cell_atlas_coords(layer, location) != Vector2i(-1, -1):
+func dig(location: Vector2i, layer: int):
+	var checked_cell: Vector2i = get_cell_atlas_coords(layer, location)
+	if checked_cell != Vector2i(-1, -1):
+		GlobalData.change_money_with_pos(get_cell_tile_data(layer, location).get_custom_data("price"), location * 16)
 		erase_cell(layer, location)
-		GlobalData.change_money_with_pos(price, location * 16)
