1f3e0cdb504849d01df121e6f126332d2eee5b0d
Jcodefox <jcodefox@gmail.com>
2023-08-16 13:20:52 -0400

    Worked on code for placing doors. Everything is buggy in this commit.

--- scripts/building.gd
+++ scripts/building.gd
@@ -5,25 +5,45 @@ var height: float = 3.0
 var mouse_down_pos: Vector3 = Vector3.ZERO
 var was_mouse_pressed: bool = false
 
+var place_mode: bool = false
+var cursor: Node3D
+var door_prefab := preload("res://prefabs/door.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	visible = not is_placeholder
+	if is_placeholder:
+		cursor = $"../Cursor"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	if not is_placeholder:
 		if Input.is_action_just_pressed("dig"):
 			var result := raycast(was_mouse_pressed, 8)
+			print(result)
 			if result.has("collider"):
 				result["collider"].queue_free()
 		return
-	if Input.is_action_just_pressed("scroll_up"):
-		height += 1.0
-	if Input.is_action_just_pressed("scroll_down"):
-		height -= 1.0
-		height = max(height, 1.0)
+	if Input.is_action_just_pressed("interact"):
+		place_mode = not place_mode
+	if not place_mode:
+		return
+	
+	var tmp_result := raycast(was_mouse_pressed, 4 if was_mouse_pressed else 2)
+	if tmp_result.has("position"):
+		cursor.position = tmp_result["position"]
+		if tmp_result["normal"] != Vector3.UP and tmp_result["normal"] != Vector3.DOWN:
+			#cursor.look_at(cursor.position + tmp_result["normal"])
+			pass
+		else:
+			cursor.look_at(cursor.position + tmp_result["normal"], Vector3.FORWARD)
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		visible = true
+		if Input.is_action_just_pressed("scroll_up"):
+			height += 1.0
+		if Input.is_action_just_pressed("scroll_down"):
+			height -= 1.0
+			height = max(height, 1.0)
 		var result := raycast(was_mouse_pressed, 4 if was_mouse_pressed else 2)
 		if result.has("position"):
 			if not was_mouse_pressed:
@@ -37,8 +57,6 @@ func _physics_process(delta):
 			position.z = (corner_a.z + corner_b.z) * 0.5
 			$CSGBox3D.size += Vector3(0, height, 0)
 			$CSGBox3D2.size = $CSGBox3D.size - Vector3(0.1, 0.2, 0.1)
-			$Door.position.y = -$CSGBox3D.size.y * 0.5 + $Door.size.y * 0.5
-			$Door.position.z = $CSGBox3D.size.z * 0.5
 			position.y = abs(mouse_down_pos.y + $CSGBox3D.size.y * 0.5)
 			$PlacementArea.global_position.y = mouse_down_pos.y
 			$PlacementArea/CollisionShape3D.shape.size = abs(Vector3($CSGBox3D.size.x * 2, 0.01, $CSGBox3D.size.z * 2)) + Vector3(10.0, 0.0, 10.0)
@@ -46,12 +64,21 @@ func _physics_process(delta):
 	else:
 		if was_mouse_pressed:
 			var new_building := duplicate(7)
-			new_building.use_collision = true
 			new_building.get_node("PlacementArea").queue_free()
 			new_building.is_placeholder = false
-			get_parent().add_child(new_building)
+			get_node("../CSGCombiner3D").add_child(new_building)
 			visible = false
 		was_mouse_pressed = false
+	if Input.is_action_just_pressed("jump"):
+		var result := raycast(was_mouse_pressed, 4 if was_mouse_pressed else 2)
+		if result.has("position"):
+			var new_door := door_prefab.instantiate()
+			new_door.position = result["position"]
+			get_node("../CSGCombiner3D").add_child(new_door)
+			if tmp_result["normal"] != Vector3.UP and tmp_result["normal"] != Vector3.DOWN:
+				new_door.look_at(cursor.position + tmp_result["normal"])
+			else:
+				new_door.look_at(cursor.position + tmp_result["normal"], Vector3.FORWARD)
 
 func raycast(collide_with_areas: bool, mask: int) -> Dictionary:
 	var camera: Camera3D = get_viewport().get_camera_3d()
